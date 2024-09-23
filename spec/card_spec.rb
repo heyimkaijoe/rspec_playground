df5a3610a0119d3741970176f6d11b0a6309bb54
kchsu <kevin3117213@gmail.com>
2024-09-24 00:51:57 +0800

    Refactor: argument in describe from docstring to class to allow helper methods

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -6,7 +6,7 @@ class Card
   end
 end
 
-RSpec.describe 'Card' do # an example group
+RSpec.describe Card do # an example group
   it 'has a type' do # an example
     card = Card.new('Ace of Spades')
     expect(card.type).to eq('Ace of Spades') # an assertion
