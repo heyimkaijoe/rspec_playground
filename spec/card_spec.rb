186c3123dabab423c679f2ddef17c9d36e330040
kchsu <kevin3117213@gmail.com>
2024-09-24 00:44:33 +0800

    Add: first assertion

---
+++ spec/card_spec.rb
@@ -0,0 +1,14 @@
+class Card
+  attr_reader :type
+
+  def initialize(type)
+    @type = type
+  end
+end
+
+RSpec.describe 'Card' do # an example group
+  it 'has a type' do # an example
+    card = Card.new('Ace of Spades')
+    expect(card.type).to eq('Ace of Spades') # an assertion
+  end
+end
