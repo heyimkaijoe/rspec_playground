193e4e18a3430fc97a0b78996d2e7aefc19e2cfb
kchsu <kevin3117213@gmail.com>
2024-09-25 16:09:40 +0800

    Refactor: duplication utilizing helper method let

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,5 @@
 class Card
-  attr_reader :rank, :suit
+  attr_accessor :rank, :suit
 
   def initialize(rank, suit)
     @rank = rank
@@ -8,16 +8,20 @@ class Card
 end
 
 RSpec.describe Card do # an example group
-  # helper method
-  def card
-    Card.new('Ace', 'Spades')
-  end
+  # helper method 'let'
+    # separation between different examples
+    # memoization(cache) in the example
+    # lazy loading
+      # use let! instead to have eager loading
+  let(:card) { Card.new('Ace', 'Spades') }
 
   it 'has a rank' do # an example
     expect(card.rank).to eq('Ace') # an assertion
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
   end
 
   it 'has a suit' do
-    expect(card.suit).to eq('Spades') # same attributes but different object (different memory) in every example
+    expect(card.suit).to eq('Spades')
   end
 end
